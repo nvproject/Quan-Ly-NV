af9985dbc1b4d4e79aa0087476d00bbf07603ac5
cuc2004 <dtc225200170@ictu.edu.vn>
2025-07-27 18:28:39 +0700

    app.py

--- app.py
+++ app.py
@@ -138,4 +138,5 @@ if __name__ == "__main__":
     env = os.environ.get('FLASK_ENV', 'development')
     port = int(os.environ.get('PORT', 5000))
     debug = False if env == 'production' else True
-    app.run(port=port, debug=debug)
+    app.run(host='0.0.0.0', port=port, debug=debug)
+
